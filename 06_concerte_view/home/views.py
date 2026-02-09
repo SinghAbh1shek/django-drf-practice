2af075131ad961dfe0e82dea36b4a5a64782824e
Abhishek Singh <abhisheksingh.muj@gmail.com>
2026-02-09 14:33:41 +0530

    learned actions

--- 06_concerte_view/home/views.py
+++ 06_concerte_view/home/views.py
@@ -1,6 +1,8 @@
 from .serializers import StudentSerializer
 from .models import Student
 from rest_framework import generics, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class StudentListCreate(generics.ListCreateAPIView):
     queryset = Student.objects.all()
@@ -9,3 +11,20 @@ class StudentListCreate(generics.ListCreateAPIView):
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+    @action(detail=False, methods=['POST'])
+    def export_student(self, request):
+        return Response({
+            'status': True,
+            'message': 'file exported',
+            'data': {}
+        })
+    
+    @action(detail=True, methods=['POST'])
+    def send_email_student(self, request, pk):
+        print(f'email sent - {pk}')
+        return Response({
+            'status': True,
+            'message': f'email sent {pk}',
+            'data': {}
+        })
