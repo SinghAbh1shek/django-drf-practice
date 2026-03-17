603619cac44df0a56592e885daee24250edd9f3e
Abhishek Singh <abhisheksingh.muj@gmail.com>
2026-03-17 13:50:05 +0530

    added global throttling

--- 11_throttling/core/settings.py
+++ 11_throttling/core/settings.py
@@ -117,3 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/day',
+        'user': '20/day'
+    }
+}
