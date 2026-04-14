d4c5f91600604e6d45e1be5cea6a1cf368a858b5
Abhishek Singh <abhisheksingh.muj@gmail.com>
2026-04-14 16:32:59 +0530

    enabled cors

--- 12_authentication/backend/home/views.py
+++ 12_authentication/backend/home/views.py
@@ -3,13 +3,13 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 
 class HomeAPI(APIView):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     def get(self, request):
         return Response({
             'status': True,
             'message': 'home page fetched',
             'data': {
-                'user': request.user.first_name,
+                # 'user': request.user.first_name,
                 'message': "Hey There!"
             }
         })
