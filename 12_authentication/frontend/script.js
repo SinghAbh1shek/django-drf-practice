2ec49a54f889c0433576059d28e993ccc615d1ef
Abhishek Singh <abhisheksingh.muj@gmail.com>
2026-04-18 14:43:43 +0530

    fix - authorization

--- 12_authentication/frontend/script.js
+++ 12_authentication/frontend/script.js
@@ -1,18 +1,31 @@
-// const getData = async () => {
-//     const res = await fetch('http://127.0.0.1:8000/api/home/')
-//     return  res.json()
-// }
-// // console.log(getData())
-
-// const result = await getData()
-// console.log(result())
-
-// const getData = fetch('http://127.0.0.1:8000/api/home/')
-// console.log(getData)
-
-
-async function getData() {
-    const res = await fetch('http://127.0.0.1:8000/api/home/')
-    console.log(await res.json())
+const user = document.getElementById('user')
+const token = localStorage.getItem('token')
+if (!token) {
+    document.location.replace('login/login.html')
 }
+async function getData() {
+    try {
+        const response = await fetch('http://127.0.0.1:8000/api/home/', {
+            'method': 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${token}`
+            },
+        })
+        if (response.status === 401) {
+            localStorage.removeItem('token')
+            document.location.replace('login/login.html')
+            return null
+        }
+        const data = await response.json()
+        user.innerText = data.data.user ? data.data.user + "👋" : "User 👋"
+        console.log(data)
+        console.log(data.data.user)
+
+    }
+    catch (error) {
+        document.location.href = 'login/login.html'
+    }
+}
+
 getData()
