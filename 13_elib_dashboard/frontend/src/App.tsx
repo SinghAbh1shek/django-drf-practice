fe6b7ade1839d116815e3113d82db02223423117
Abhishek Singh <abhisheksingh.muj@gmail.com>
2026-04-26 18:44:35 +0530

    setup shadcn ui

--- 13_elib_dashboard/frontend/src/App.tsx
+++ 13_elib_dashboard/frontend/src/App.tsx
@@ -1,9 +1,10 @@
-function App () {
+import { Button } from "@/components/ui/button"
 
+function App() {
   return (
-    <>
-    <h1>Hey There!</h1>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
