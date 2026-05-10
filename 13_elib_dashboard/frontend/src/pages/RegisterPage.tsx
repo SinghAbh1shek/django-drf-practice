68d1f076e98dd6cb690206c1fb6822d316b40389
Abhishek Singh <abhisheksingh.muj@gmail.com>
2026-05-10 16:04:45 +0530

    add register functionality

--- 13_elib_dashboard/frontend/src/pages/RegisterPage.tsx
+++ 13_elib_dashboard/frontend/src/pages/RegisterPage.tsx
@@ -13,12 +13,45 @@ import {
   FieldLabel,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
-import { Link } from "react-router"
+import { register } from "@/http/api"
+import { useMutation } from "@tanstack/react-query"
+import { LoaderCircle } from "lucide-react"
+import { useRef } from "react"
+import { Link, useNavigate } from "react-router"
 
 
 
-const RegisterPage = () =>  
-{
+const RegisterPage = () => 
+  { 
+
+  const navigate = useNavigate()
+
+  const emailRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+  const fullnameRef = useRef<HTMLInputElement>(null)
+
+  const mutation = useMutation({
+    mutationFn: register,
+    onSuccess: () => {
+      console.log('register success')
+
+      // redirect to dashboard
+      navigate('/dashboard/home')
+    }
+  })
+
+  const handleRegisterSubmit = () => {
+    const email = emailRef.current?.value
+    const password = passwordRef.current?.value
+    const fullname = fullnameRef.current?.value
+
+    if (!fullname || !email || !password){
+      return alert('please enter email and password')
+    }
+    mutation.mutate({email, password, full_name:fullname})
+  }
+
+
   return (
     <div className="flex justify-center items-center h-screen">
       <Card className="w-full max-w-sm">
@@ -26,6 +59,7 @@ const RegisterPage = () =>
           <CardTitle className="text-xl">Create your account</CardTitle>
           <CardDescription>
             Enter your email below to create your account
+            {mutation.isError && <div className="text-red-500 text-sm">{mutation.error.message}</div>}
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -33,11 +67,12 @@ const RegisterPage = () =>
             <FieldGroup>
               <Field>
                 <FieldLabel htmlFor="name">Full Name</FieldLabel>
-                <Input id="name" type="text" placeholder="John Doe" required />
+                <Input ref={fullnameRef} id="name" type="text" placeholder="John Doe" required />
               </Field>
               <Field>
                 <FieldLabel htmlFor="email">Email</FieldLabel>
                 <Input
+                  ref={emailRef}
                   id="email"
                   type="email"
                   placeholder="m@example.com"
@@ -47,14 +82,17 @@ const RegisterPage = () =>
               <Field>
                 <Field>
                     <FieldLabel htmlFor="password">Password</FieldLabel>
-                    <Input id="password" type="password" required />
+                    <Input ref={passwordRef} id="password" type="password" required />
                 </Field>
                 <FieldDescription>
                   Must be at least 8 characters long.
                 </FieldDescription>
               </Field>
               <Field>
-                <Button type="submit">Create Account</Button>
+                <Button type="button" onClick={handleRegisterSubmit} disabled={mutation.isPending}>
+                  {mutation.isPending && <LoaderCircle className="animate-spin"/>}
+                  <span>Create Account</span>
+                </Button>
                 <FieldDescription className="text-center">
                   Already have an account? <Link to={'/auth/login'}>Sign in</Link>
                 </FieldDescription>
