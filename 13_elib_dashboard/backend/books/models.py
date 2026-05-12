2de4c8a4c23ddf4455499d8770fd87902123f964
Abhishek Singh <abhisheksingh.muj@gmail.com>
2026-05-12 17:30:28 +0530

    add book and author model

---
+++ 13_elib_dashboard/backend/books/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    coverImage = models.URLField()
+    file = models.URLField()
+    genre = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
