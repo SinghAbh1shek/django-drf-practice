c98815696b3bb9877d9acaec7d18a720dcecbc45
Abhishek Singh <abhisheksingh.muj@gmail.com>
2026-05-12 18:37:16 +0530

    add book api

--- 13_elib_dashboard/backend/books/views.py
+++ 13_elib_dashboard/backend/books/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookApiView(APIView):
+    def get(self, request):
+        try:
+            queryset = Book.objects.all()
+            serializer = BookSerializer(queryset, many=True)
+            return Response({
+                'status': False,
+                'message': 'record fetched', 
+                'data': serializer.data
+            })
+            
+        except Exception as e:
+            return Response({
+                'status': False,
+                'message': 'something went wrong', 
+                'data': str(e)
+            }, status=500)
